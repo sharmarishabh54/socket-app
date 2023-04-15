4f6f0fa9b27f969b53a1c9936d635b6bf5433b3e
Rishabh Sharma <rishabh.sharma@fortheye.co>
2023-04-15 20:10:43 +0530

    SocketTestCode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const httpServer = require("http").createServer();
+const io = require("socket.io")(httpServer, {
+    cors: {
+      origin: "http://localhost:8080",
+    },
+  });
+  
+  const crypto = require("crypto");
+  const randomId = () => crypto.randomBytes(8).toString("hex");
+  
+  const { InMemorySessionStore } = require("./sessionStore");
+  const sessionStore = new InMemorySessionStore();
+  
+  const { InMemoryMessageStore } = require("./messageStore");
+  const messageStore = new InMemoryMessageStore();
+  
 
 const apiRoutes = require("./index.routes.js");
 dotenv.config();
@@ -27,86 +43,109 @@ app.use((req, res, next) => {
     next();
 });
 
-const server = app.listen(process.env.PORT, () =>
-    console.log(`application running on http://${process.env.HOST}:${process.env.PORT}`)
-);
-
-const io = require("socket.io")(server, {
-    pingTimeout: 60000,
-    cors: {
-        origin: "http://localhost:3000",
-    },
-});
-
-io.on("connection", (socket) => {
-    console.log("Connected to socket.io");
-
-    socket.on("message", (userData)=>{
-        socket.join(userData._id);
-        console.log("userData._id", userData._id);
-        socket.emit("connected");
+io.use((socket, next) => {
+    const sessionID = socket.handshake.auth.sessionID;
+    if (sessionID) {
+      const session = sessionStore.findSession(sessionID);
+      if (session) {
+        socket.sessionID = sessionID;
+        socket.userID = session.userID;
+        socket.username = session.username;
+        return next();
+      }
+    }
+    const username = socket.handshake.auth.username;
+    if (!username) {
+      return next(new Error("invalid username"));
+    }
+    socket.sessionID = randomId();
+    socket.userID = randomId();
+    socket.username = username;
+    next();
+  });
+  
+  io.on("connection", (socket) => {
+    // persist session
+    sessionStore.saveSession(socket.sessionID, {
+      userID: socket.userID,
+      username: socket.username,
+      connected: true,
     });
-
-    socket.on('join chat', (room) => {
-        socket.join(room);
-        console.log("user jooned room: " + room);
+  
+    // emit session details
+    socket.emit("session", {
+      sessionID: socket.sessionID,
+      userID: socket.userID,
     });
-
-    socket.on('typing',(room)=> socket.in(room).emit("typing"));
-    socket.on("stop typing", (room)=> socket.in(room).emit("typing"));
-
-    socket.on("new message", (newMessageRecieved) => {
-        const chat = newMessageRecieved.chat;
-
-        if(!chat.users) return console.log("cht");
-
-        chat.users.forEach(user => {
-            if (user._id == newMessageRecieved.sender._id) return;
-
-            socket.in(user, _id).emit("message recieved", newMessageRecived);
-        })
+  
+    // join the "userID" room
+    socket.join(socket.userID);
+  
+    // fetch existing users
+    const users = [];
+    const messagesPerUser = new Map();
+    messageStore.findMessagesForUser(socket.userID).forEach((message) => {
+      const { from, to } = message;
+      const otherUser = socket.userID === from ? to : from;
+      if (messagesPerUser.has(otherUser)) {
+        messagesPerUser.get(otherUser).push(message);
+      } else {
+        messagesPerUser.set(otherUser, [message]);
+      }
     });
+    sessionStore.findAllSessions().forEach((session) => {
+      users.push({
+        userID: session.userID,
+        username: session.username,
+        connected: session.connected,
+        messages: messagesPerUser.get(session.userID) || [],
+      });
+    });
+    socket.emit("users", users);
+  
+    // notify existing users
+    socket.broadcast.emit("user connected", {
+      userID: socket.userID,
+      username: socket.username,
+      connected: true,
+      messages: [],
+    });
+  
+    // forward the private message to the right recipient (and to other tabs of the sender)
+    socket.on("private message", ({ content, to }) => {
+      const message = {
+        content,
+        from: socket.userID,
+        to,
+      };
+      socket.to(to).to(socket.userID).emit("private message", message);
+      messageStore.saveMessage(message);
+    });
+  
+    // notify users upon disconnection
+    socket.on("disconnect", async () => {
+      const matchingSockets = await io.in(socket.userID).allSockets();
+      const isDisconnected = matchingSockets.size === 0;
+      if (isDisconnected) {
+        // notify other users
+        socket.broadcast.emit("user disconnected", socket.userID);
+        // update the connection status of the session
+        sessionStore.saveSession(socket.sessionID, {
+          userID: socket.userID,
+          username: socket.username,
+          connected: false,
+        });
+      }
+    });
+  });
+  
+  const PORT = process.env.PORT || 3000;
+  
+  httpServer.listen(PORT, () =>
+    console.log(`server listening at http://localhost:${PORT}`)
+  );
+  
 
-    socket.off("message", () => {
-        console.log("User Disconnected");
-        socket.leave(userData._id);
-    })
-});
 
 
 
-// const httpServer = createServer((req, res) => {
-//   if (req.url !== "/") {
-//     res.writeHead(404);
-//     res.end("Not found");
-//     return;
-//   }
-// });
-
-// const io = new Server(httpServer, {
-//   cors: {
-//     origin: 'http://localhost:3000'
-//   }
-// });
-
-// io.on("connection", async (socket) => {
-//   const count = io.engine.clientsCount;
-//   console.log(`connect ${socket.id} with present user ${count}`);
-
-//   // socket.use(([event, ...args], next) => {
-//   //   console.log('event::', event);
-//   //   console.log('args::', ...args);
-//   // });
-
-//   socket.on('message', (event, args) => {
-//     socket.broadcast.emit('recieve-message', event, args)
-//     console.log('data', event);
-//     console.log('args', args);
-//   });
-
-//   socket.on("disconnect", (reason) => {
-//     console.log(`disconnect ${socket.id} due to ${reason}`);
-//   });
-// });
-
-// httpServer.listen(process.env.PORT);
